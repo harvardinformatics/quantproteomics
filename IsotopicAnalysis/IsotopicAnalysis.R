0d6296a925fd20ae3157ce9fac56c7d6f07559ad
Alexandria D'Souza <dsouzaallie@gmail.com>
2021-03-25 13:27:42 -0400

    mzML MS Scan info to csv,txt

--- IsotopicAnalysis/IsotopicAnalysis.R
+++ IsotopicAnalysis/IsotopicAnalysis.R
@@ -3,10 +3,15 @@ library(mzR)
 fileMS <- system.file()
 MSdata <- openMSfile('SUB09034_SPL007.mzML')
 fileName(MSdata)
-runInfo(MSdata)
+y <- runInfo(MSdata)
 
-head(peaks(MSdata))
+getScan <- function(msScan, scanNum, retentionTime) {
+  getData<-peaks(msScan, scan=scanNum)
+  
+}
 
-
-
-#write.table(head(peaks(MSdata)),'SUB09034_SPL007.txt')
+x<-getScan(MSdata, 4, 71.45420)
+x
+#x[1,][1]
+write.table(y,'SUB09034_SPL007_info.txt', col.names = F, row.names = F)
+write.table(x,'SUB09034_SPL007.txt', col.names = F, row.names = F)
