0a101ff07b8feb2e73e13c96b883e59ce2f60db5
Alexandria D'Souza <dsouzaallie@gmail.com>
2021-03-29 20:27:54 -0400

    add args for terminal, formula/adduct/ionmode

--- IsotopicAnalysis/IsotopeAnalysis.py
+++ IsotopicAnalysis/IsotopeAnalysis.py
@@ -1,3 +1,5 @@
+#run on terminal with python3 IsotopeAnalysis.py 'C4 H7 N O4'
+
 import sys
 
 Compound=sys.argv[1]
@@ -8,10 +10,6 @@ def createEleDict():
 	'Sc':44.9559,'Ti':47.90,'V':50.9415,'Cr':51.996,'Mn':54.9380,'Fe':55.847,'Ni':58.70,'Co':58.9332,'Cu':63.546,'Zn':65.38,'Ga':69.72,
 	'Ge':72.59,'As':74.9,'Se':78.96,'Br':79.904,'Kr':83.80,'Rb':85.4678,'Sr':87.62,'Y':88.9059,'Zr':91.22,'Nb':92.9064,'Mo':95.94,
 	'Tc':98,'Ru':101.07,'Rh':102.9055,'Pd':106.4,'Ag':107.868,'Cd':112.41,'In':114.82,'Sn':118.69,'Sb':121.75,'I':126.9045,'Te':127.60}
-	#'Xe':131.30,'Cs':132.9054,'Ba':137.33,'La':138.9055,'Ce':140:12,'Pr':140.9077,'Nd':144.24,'Pm':145,'Sm':150.4,'Eu':151.96,'Gd':151.96,
-	#'Td':158.9254,'Dy':162.50,'Ho':164.9304,'Er':167.26,'Tm':168.9342,'Yb':173.04,'Lu':174.967,'Hf':178.49,'Ta':180.9479,'W':183.85,'Re':186.207,
-	#'Os':190.2,'Ir':192.22,'Pt':195.09,'Au':196.9665,'Hg':200.59,'Tl':204.37,'Pb':207.2,'Bi':208.9804,'Po':209,'At':210,'Rn':222,'Fr':223,
-	#'Ra':226.0254,'Ac':227.0278,'Pa':231.0359,'Th':232.0381,'Np':237.0482,'U':238.029}
 	return elementDict
 
 
@@ -28,8 +26,7 @@ def createIsotopes(cmp, eleDict):
 				ele=element[:element.find(str(x))]
 				eleNum=int(element[element.find(str(x)):])
 				mz+=eleDict[ele]*eleNum
-				#print(element[:element.find(str(x))])
-				#print(element[element.find(str(x)):])
+
 				
 				if 'C' in element:
 					C=eleNum
